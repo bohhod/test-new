76b10559bee26d226573671c535bd2e68d5c8120
bohhod <bohhod@i.ua>
2024-12-05 04:55:58 +0200

    second commit

--- 10_lesson/requests_study.py
+++ 10_lesson/requests_study.py
@@ -1,3 +1,12 @@
+# git init
+# git add .
+# git config --global user.email "bohhod@i.ua" 
+# git config --global user.name "bohhod"
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/bohhod/test-new.git
+# git push -u origin main
+
 import requests
 import json
 
